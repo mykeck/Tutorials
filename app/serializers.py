35f351eba7ac56e3229698177106a4ee14fd6577
MikeC <kiragumike@gmail.com>
2020-10-29 14:15:30 +0300

    adde components

--- app/serializers.py
+++ app/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
-from models import Articles
+from .models import Articles
 
 
 
-class ArticlesSerializer(serializers.Serializer):
+class ArticlesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Articles
-        fields = ("title", "author" "email","date")
+        fields = ("title", "author", "email","date","pk")
 
     def create(self, validate_data):
         return Articles.objects.create(validated_data)
 
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get()    
+        instance.title = validated_data.get('title',instance.title)
+        instance.author = validated_data.get('author',instance.author) 
+        instance.email = validated_data.get('email',instance.author) 
+        instance.date = validated_data.get('date',instance.author)   
+        instance.save()
+        return instance 
     
--- app/views.py
+++ app/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Articles
+from .serializers import ArticlesSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Articles.objects.all()
+        serializer = ArticlesSerializer(articles,many = True)
+        return  JsonResponse(serializer.data, safe = False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticlesSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)  
+
+
+@csrf_exempt
+def article_detail(request,pk):
+    try: 
+        article = Articles.objects.get(pk=pk) 
+
+    except Articles.DoesNotExist:
+        return HttpResponse(status = 404)
+
+    if request.method == 'GET':
+        serializer = ArticlesSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticlesSerializer(article,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status = 400) 
+
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)     
+
